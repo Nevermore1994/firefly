31d22b9f3f08e8eb2463f7a62fb091019701792b
Nevermore <ping.tan@shopee.com>
2022-04-14 18:14:46 +0800

    update

--- core/network/NetworkConfig.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Nevermore on 2021/11/26.
-// firefly NetworkConfig
-// Copyright (c) 2021 Nevermore All rights reserved.
-//
-#pragma once
-
-#define NETWORK_LOG 0
--- core/network/UdpLink.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by Nevermore on 2021/12/6.
-// firefly UdpLink
-// Copyright (c) 2021 Nevermore All rights reserved.
-//
-#include "UdpLink.hpp"
