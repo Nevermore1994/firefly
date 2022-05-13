698db048c6e9fac584b8e59e06e2d749a21574e8
Nevermore <ping.tan@shopee.com>
2022-05-13 22:30:51 +0800

    support reflection

---
+++ firefly/Utility/headers/Reflection.hpp
@@ -0,0 +1,60 @@
+//
+// Created by Nevermore on 2022/5/13.
+// firefly Reflection
+// Copyright (c) 2022 Nevermore All rights reserved.
+//
+
+#pragma once
+#include <unordered_map>
+#include <string>
+#include <functional>
+#include <any>
+#include <mutex>
+#include "NoCopyable.hpp"
+
+namespace firefly{
+
+using ReflectionFunction = std::function<void*(void)>;
+
+class Reflection {
+public:
+    static Reflection& shareInstance() noexcept {
+        static Reflection instance;
+        return instance;
+    }
+    
+    inline ReflectionFunction generate(const std::string& name) noexcept {
+        std::unique_lock<std::mutex> lock(mutex_);
+        auto it = builders_.find(name);
+        if(it == builders_.end()) {
+            return nullptr;
+        }
+        return it->second;
+    }
+    
+    inline void enrolment(std::string name, ReflectionFunction func) noexcept {
+        std::unique_lock<std::mutex> lock(mutex_);
+        builders_.insert_or_assign(std::move(name), std::move(func));
+    }
+
+private:
+    Reflection() = default;
+    
+    ~Reflection() = default;
+
+private:
+    std::unordered_map<std::string, ReflectionFunction> builders_;
+    std::mutex mutex_;
+};
+
+template<typename T>
+void registerClass(const std::string& name);
+
+void* generate(const std::string& name);
+
+#define RegisterClass(name) \
+    registerClass<name>(#name)
+
+#define GenerateClass(name) \
+    generate(name)
+}
